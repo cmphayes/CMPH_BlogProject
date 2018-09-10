852f173ff297cac86daa591a4fe86dd4a6d9e018
cmphayes <42660033+cmphayes@users.noreply.github.com>
2018-09-10 16:42:53 -0400

    Add project files.

---
+++ CMPH_BlogProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CMPH_BlogProject.Startup))]
+namespace CMPH_BlogProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
