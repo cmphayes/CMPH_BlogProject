81d81fc80c4d398a55b1d0ddd51760edbfd26234
cmphayes <42660033+cmphayes@users.noreply.github.com>
2018-10-23 16:52:19 -0400

    added google login
    Added 1 3rd party login. need to add login from linked in and github

---
+++ CMPH_BlogProject/Helper/BlogHelper.cs
@@ -0,0 +1,29 @@
+﻿using CMPH_BlogProject.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CMPH_BlogProject.Helper
+{
+    public class BlogHelper
+    {
+        private UserManager<ApplicationUser> UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+        private static ApplicationDbContext db = new ApplicationDbContext();
+
+        public static string GetBlogImagePath(int blogId)
+        {
+        //    var defaultPath = "/Uploads/default.jpg";
+        //    if (string.IsNullOrEmpty(blogId))
+        //        return defaultPath;
+
+            var blogImagePath = db.Blog.Find(blogId).MediaURL;
+            if (string.IsNullOrEmpty(blogImagePath))
+                return blogImagePath;
+
+            return blogImagePath;
+        }
+    }
+}
